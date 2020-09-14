cedc9c52bc84b6197a00d7ff4c41c46082c3d41c
yekhaung <yekhaung.dev@gmail.com>
2020-09-14 12:25:18 +0630

    add ingress exp

--- python-k8s/config/pgconfig.py
+++ python-k8s/config/pgconfig.py
@@ -1,25 +1,14 @@
-import os
+import yaml
+from os import path, environ
 
-K8S_CONTEXT = os.environ.get("K8S_CONTEXT", "docker-desktop")
-K8S_NAMESPACE = os.environ.get("K8S_NAMESPACE", "test")
+K8S_CONTEXT = environ.get("K8S_CONTEXT", "docker-desktop")
+K8S_NAMESPACE = environ.get("K8S_NAMESPACE", "test")
 
-INGRESS_NAME = os.environ.get("INGRESS_NAME", "sample-ingress")
-DOWNSTREAM_SERVICE = "sample-shadow-service"
-NGINX_MIRROR_ANNOTATION = {
-    "nginx.org/location-snippets": "mirror  /mirror;\n",
-    "nginx.org/server-snippets": 'resolver kube-dns.kube-system valid=10s;\n\
-        location = /mirror {\n    \
-        internal;\n    set $shadow_service_name "{DOWNSTREAM_SERVICE}";\n    \
-        proxy_set_header X-Mirror-Request  true;\n    \
-        proxy_set_header X-Shadow-Service  \
-        $shadow_service_name;\n    \
-        proxy_set_header X-Real-IP  \
-        $remote_addr;\n    \
-        proxy_set_header X-Forwarded-For  \
-        $proxy_add_x_forwarded_for;\n    \
-        proxy_set_header Host  $host;\n    \
-        proxy_pass http://{DOWNSTREAM_SERVICE}$request_uri;\n}\n',
-}
+INGRESS_NAME = environ.get("INGRESS_NAME", "hello-ingress")
+DOWNSTREAM_SERVICE = "hello-shadow-service"
+with open(path.join(path.dirname(__file__), "nginx-mirror.yaml")) as f:
+    conf = yaml.safe_load(f)
+    NGINX_MIRROR_ANNOTATION = conf
 
 DEPLOYEMNT_NAME = "hello-app-deployment"
 IMAGE_V2 = "gcr.io/google-samples/hello-app:1.0"
--- python-k8s/controller.py
+++ python-k8s/controller.py
@@ -5,43 +5,73 @@ from config.pgconfig import (
     K8S_NAMESPACE,
     DEPLOYEMNT_NAME,
     IMAGE_V2,
+    INGRESS_NAME,
+    NGINX_MIRROR_ANNOTATION,
 )
 
 
 class K8sController:
     """
-    CRUD k8s resources with python client
+    Calling k8s API with python client
+
+    For process to be run in k8s cluster, use load_incluster_config()
+
+    For client-side, e.g local machine. use load_kube_config() to read k8s
+    configuration from local `.kube` path
     """
 
     def __init__(self, context=K8S_CONTEXT, namespace=K8S_NAMESPACE):
         self.ns = namespace
         self.ctx = context
 
-        # load kube config
-        config.load_kube_config(context=self.ctx)
-        print(f"Selected context: {self.ctx}")
+        # config.load_kube_config(context=self.ctx)
+        # print(f"Selected context: {self.ctx}")
+        config.load_incluster_config()
 
         self.app_api = client.AppsV1Api()
         self.core_api = client.CoreV1Api()
         self.net_api = client.NetworkingV1beta1Api()
 
-    def update(self, name, resource):
-        print(f"Updating config of [{name}]...")
-        resource.spec.template.spec.containers[0].image = IMAGE_V2
-        res = self.app_api.patch_namespaced_deployment(
-            name=name, namespace=self.ns, body=resource
-        )
-        print(f"Resource updated. status={res.status}")
-
-    def run(self):
-        print(f"Listing deployments from [{self.ns}] namespace...")
+    def update_deployemnt(self, name):
+        print(f"Finding deployment:{name} in [{self.ns}] namespace...")
         res = self.app_api.list_namespaced_deployment(self.ns)
         for item in res.items:
-            if item.metadata.name == DEPLOYEMNT_NAME:
-                print(f"Found target resource. name={item.metadata.name},")
-                self.update(name=DEPLOYEMNT_NAME, resource=item)
+            print(
+                f"NAME:{item.metadata.name}, READY: {item.status.ready_replicas}/{item.status.available_replicas}"
+            )
+            if item.metadata.name == name:
+                print(
+                    f"Found target resource. name={item.metadata.name}. Updating deployment..."
+                )
+                item.spec.template.spec.containers[0].image = IMAGE_V2
+                res = self.app_api.patch_namespaced_deployment(
+                    name=name, namespace=self.ns, body=item
+                )
+                print(
+                    f"Deployment updated. status={res.status.ready_replicas}/{res.status.available_replicas}"
+                )
                 return
-        print("No deployment found.")
+
+    def update_ingress(self, name):
+        print(f"Finding ingress:{name} in [{self.ns}] namespace...")
+        res = self.net_api.list_namespaced_ingress(self.ns)
+        for item in res.items:
+            print(f"NAME:{item.metadata.name}, HOSTS:{item.spec.rules}")
+            if item.metadata.name == name:
+                print(
+                    f"Found target resource. name={item.metadata.name}. Updating ingress ..."
+                )
+                # item.metadata.annotations = {"nginx.org/location-snippets": "", "nginx.org/server-snippets": ""}
+                item.metadata.annotations = NGINX_MIRROR_ANNOTATION
+                res = self.net_api.patch_namespaced_ingress(
+                    name=name, namespace=self.ns, body=item
+                )
+                print(f"Ingress updated. {res.status}")
+                return
+
+    def run(self):
+        self.update_deployemnt(DEPLOYEMNT_NAME)
+        self.update_ingress(INGRESS_NAME)
 
 
 if __name__ == "__main__":
